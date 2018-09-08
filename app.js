4d3e2ca3c831896ea9e43c294eb326771977d4eb
Oliver Dyla <neutrino84@gmail.com>
2018-09-08 11:12:32 +0200

    added about, contact, and services pages

--- app.js
+++ app.js
@@ -14,9 +14,28 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(req, res) {
   res.render('index', {
-  	title: 'STM BioMedical'
+    title: 'STM BioMedical | Home'
   });
 });
+
+app.get('/about', function(req, res) {
+  res.render('about', {
+    title: 'STM BioMedical | About'
+  });
+});
+
+app.get('/services', function(req, res) {
+  res.render('services', {
+    title: 'STM BioMedical | Services'
+  });
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact', {
+    title: 'STM BioMedical | Contact'
+  });
+});
+
 
 // start server
 app.listen(4000, function() {
